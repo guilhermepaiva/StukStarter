16eebbe101e1223ff66c17d2274ba36f92085e62
guilhermepaiva <guilhermepaiva9@gmail.com>
2016-05-28 10:18:04 -0300

    defined create action and show view for product

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,10 @@
 #
 
 class ProjectsController < ApplicationController
+
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 		@displayed_projects = Project.take(4)
@@ -31,5 +35,28 @@ class ProjectsController < ApplicationController
 	def edit
 	end
 
+	def create
+		@project = current_user.projects.build(project_params)
+		respond_to do |format|
+			if @project.save
+				format.html { redirect_to @project, notice: "Project was successfully created" }
+				format.json { render :show, status: :ok, location: @project }
+			else
+				format.html { redirect_to :edit }
+				format.json { render json: @project.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+
+	private
+
+	def set_project
+		@project = Project.find(params[:id])
+	end
+
+	def project_params
+		params.require(:project).permit(:name, :short_description, :description, :image_url, :goal, :status, :expiration_time)
+	end
 	
 end
